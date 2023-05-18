fe06cb1d167079e2b893e15438ba0c44ff3b3de6
Neko Juers <neko.juers@gmail.com>
2023-05-18 09:26:27 -0400

    Added method to extract artists from Spotify data.

---
+++ TODO.md
@@ -0,0 +1,10 @@
+# TODO
+## Ordered approximately by priority.
+
+- Testing!
+% - Scraper
+  - Done with ChatGPT--needs refactoring.
+- Disambiguation resolution
+- Vizualization
+- Dashboard
+- CSS
--- scenemap/spotify.py
+++ scenemap/spotify.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 import os 
+import requests
 from dotenv import load_dotenv
+
+from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -10,6 +13,9 @@ class Spot:
         Primary spotify object.
     """
     def __init__(self):
+        self.artists = {}
+        self.albums = {}
+
         load_dotenv()
         client_secret = os.getenv("client_secret")
         client_id = os.getenv("client_id")
@@ -26,27 +32,78 @@ class Spot:
         )
         return None
 
+    def _get_artists(self):
+        """
+            Parse Spotify API JSON into a dict of artists.
+            This will instantiate an empty dict of dicts with artists in the user's
+            Spotify saved albums as `self.artists`.
+        """
+        if not self.albums:
+            print("No albums yet. Getting albums.")
+            self.get_albums()
+        for album in self.albums:
+            for artist in album["album"]["artists"]:
+                self.artists[artist["name"].strip("'").strip('"')] = {}
+        return self.artists
+
     def get_albums(self, *args, **kwargs):
         """
-            Get all user albums.
+            Get all user albums. (Raw Spotify output)
         """
         # Handle kwarg defaults
         try:
             limit = kwargs["limit"]
         except KeyError:
             limit = 50
-        breakpoint()
         out = self.session.current_user_saved_albums(limit=limit)
-        albums = out["items"]
+        self.albums = out["items"]
         while out["next"]:
             out = self.session.next(out)
-            albums.extend(out["items"])
-        return albums
+            self.albums.extend(out["items"])
+        return self.albums
 
+    @staticmethod
+    def get_artist_details(artist_name):
+        url = f"https://en.wikipedia.org/wiki/{artist_name}"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
+        artist_details = {
+            'current_members': [],
+            'former_members': [],
+            'associated_acts': []
+        }
+
+        # Scrape current members
+        current_members_section = soup.find('th', text='Members')
+        if current_members_section:
+            for sibling in current_members_section.find_next_siblings('td'):
+                links = sibling.find_all('a')
+                for link in links:
+                    artist_details['current_members'].append(link.text)
+
+        # Scrape former members
+        former_members_section = soup.find('th', text='Past members')
+        if former_members_section:
+            for sibling in former_members_section.find_next_siblings('td'):
+                links = sibling.find_all('a')
+                for link in links:
+                    artist_details['former_members'].append(link.text)
+
+        # Scrape associated acts
+        associated_acts_section = soup.find('th', text='Associated acts')
+        if associated_acts_section:
+            for sibling in associated_acts_section.find_next_siblings('td'):
+                links = sibling.find_all('a')
+                for link in links:
+                    artist_details['associated_acts'].append(link.text)
+
+        return artist_details
 
 def main():
     session = Spot()
+    artists = session._get_artists()
+    breakpoint()
 
 
 if __name__ == "__main__":
