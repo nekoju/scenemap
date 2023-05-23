b38d5a4d2faa4b0aa4a8acfa9afc80673603de46
Neko Juers <neko.juers@gmail.com>
2023-05-22 22:01:35 -0400

    Visuzlization mostly working.

--- scenemap/spotify.py
+++ scenemap/spotify.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import itertools as it
-import os 
+import os
 import re
 import requests
 from dotenv import load_dotenv
@@ -10,10 +10,12 @@ from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+
 class Spot:
     """
-        Primary spotify object.
+    Primary spotify object.
     """
+
     def __init__(self):
         self.artists = {}
         self.albums = {}
@@ -36,9 +38,9 @@ class Spot:
 
     def _get_artists(self):
         """
-            Parse Spotify API JSON into a dict of artists.
-            This will instantiate an empty dict of dicts with artists in the user's
-            Spotify saved albums as `self.artists`.
+        Parse Spotify API JSON into a dict of artists.
+        This will instantiate an empty dict of dicts with artists in the user's
+        Spotify saved albums as `self.artists`.
         """
         if not self.albums:
             print("No albums yet. Getting albums.")
@@ -50,7 +52,7 @@ class Spot:
 
     def get_albums(self, *args, **kwargs):
         """
-            Get all user albums. (Raw Spotify output)
+        Get all user albums. (Raw Spotify output)
         """
         # Handle kwarg defaults
         try:
@@ -66,49 +68,85 @@ class Spot:
 
     @staticmethod
     def get_artist_details(artist_name):
-        url = f"https://en.wikipedia.org/wiki/{artist_name}"
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-
+        if artist_name == "Starflyer 59":
+            breakpoint()
+        url = Spot.search(artist_name)
+        print(url)
         artist_details = {
-            'current_members': [],
-            'former_members': [],
-            'associated_acts': []
+            "current_members": [],
+            "former_members": [],
+            "associated_acts": [],
         }
+        try:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "html.parser")
+        except requests.exceptions.MissingSchema:
+            print(f"No meta results for {artist_name}")
+            return artist_details
 
         # Scrape current members
         current_members_regex = re.compile(r"^(Current )?Members")
-        current_members_section = soup.find('th', text=current_members_regex)
+        current_members_section = soup.find("th", text=current_members_regex)
         if current_members_section:
-            for sibling in current_members_section.find_next_siblings('td'):
-                links = sibling.find_all('a')
+            for sibling in current_members_section.find_next_siblings("td"):
+                links = sibling.get_text().split("\n")
                 for link in links:
-                    artist_details['current_members'].append(link.text)
+                    artist_details["current_members"].append(link)
 
         # Scrape former members
-        former_members_section = soup.find('th', text='Past members')
+        former_members_section = soup.find("th", text="Past members")
         if former_members_section:
-            for sibling in former_members_section.find_next_siblings('td'):
-                links = sibling.find_all('a')
+            for sibling in former_members_section.find_next_siblings("td"):
+                # Firugre out how to wrk with list items here.
+                links = sibling.get_text().split("\n")
                 for link in links:
-                    artist_details['former_members'].append(link.text)
+                    artist_details["former_members"].append(link)
 
         # Scrape associated acts
-        associated_acts_section = soup.find('th', text='Associated acts')
+        associated_acts_section = soup.find("th", text="Associated acts")
         if associated_acts_section:
-            for sibling in associated_acts_section.find_next_siblings('td'):
-                links = sibling.find_all('a')
+            for sibling in associated_acts_section.find_next_siblings("td"):
+                links = sibling.get_text().split("\n")
                 for link in links:
-                    artist_details['associated_acts'].append(link.text)
+                    artist_details["associated_acts"].append(link)
 
-        print(f"{artist_name}: {artist_details}")
+        print(f"{artist_name}: done")
         return artist_details
+
+    @staticmethod
+    def search(query):
+        descriptors = [
+            "band",
+            "musician",
+            "songwriter",
+            "rapper",
+            "artist",
+            "composer",
+        ]
+        url = "https://en.wikipedia.org/w/api.php"
+        params = {
+            "action": "query",
+            "format": "json",
+            "list": "search",
+            "utf8": 1,
+            "srsearch": query + " band",
+        }
+        response = requests.get(url, params=params).json()["query"]["search"]
+        for entry in response:
+            for descriptor in descriptors:
+                if (
+                    descriptor in entry["snippet"].lower()
+                    or descriptor in entry["title"].lower()
+                ):
+                    return f"https://en.wikipedia.org/?curid={entry['pageid']}"
+
 
 def main():
     import matplotlib.pyplot as plt
     import networkx as nx
     from plots import NetworkGraph
     import pickle
+
     session = Spot()
     artists = session._get_artists()
 
@@ -122,9 +160,8 @@ def main():
 
     artists_sparse = {}
     for artist_name, artist_attr in artists.items():
-        if len(list(it.chain(*artist_attr.values()))) != 0:
+        if list(it.chain(artist_attr.values())):
             artists_sparse[artist_name] = artist_attr
-        
 
     ng = NetworkGraph(artists_sparse)
     g = ng.generate_graph()
@@ -138,7 +175,7 @@ def main():
     for node in g.nodes:
         colors_list.append(colors[g.nodes[node]["attr_type"]])
 
-    nx.draw_networkx(g, node_color = colors_list)
+    nx.draw_networkx(g, node_color=colors_list)
     breakpoint()
 
 
